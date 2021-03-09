de932e46d5e0311a9c4b9b49fb30c5e0dfabc635
Coen Joling <coenjoling@gmail.comgit config --global user.name CoenJ>
2021-03-09 16:52:19 +0100

    boekaanvraag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BoekenzoekenComponent } from './boekenzoeken/boekenzoeken.component';
+import { BoekaanvraagComponent } from './boekaanvraag/boekaanvraag.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'boekenzoeken', component: BoekenzoekenComponent },
+  { path: 'boekaanvraag', component: BoekaanvraagComponent },
 
 
 ];
