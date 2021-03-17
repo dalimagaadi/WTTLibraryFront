588b4c910335eeaa90b245175ba7851285ab30fc
Coen Joling <coenjoling@gmail.comgit config --global user.name CoenJ>
2021-03-17 17:46:08 +0100

    boek toevoegen uitgebreid

--- src/app/boektoevoegen/boektoevoegen.component.ts
+++ src/app/boektoevoegen/boektoevoegen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BoekServiceService} from '../service/boek/boek-service.service'
 import {Boek} from "../model/Boek";
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-boektoevoegen',
@@ -9,20 +10,36 @@ import {Boek} from "../model/Boek";
 })
 export class BoektoevoegenComponent implements OnInit {
   auteur: string;
+  titel: string;
+  isbn: string;
+  aantal: number;
+  status: string;
+  tags: string;
 
 
 
-  constructor(private _boekService: BoekServiceService) { }
+  constructor(private _boekService: BoekServiceService, private router: Router) { }
 
   ngOnInit(): void {
 }
 
 handleAddBook(){  
+  if (this.auteur==null||this.aantal==null||this.isbn==null||this.tags==null||this.titel==null){
+    alert("Er is een veld niet ingevuld. Vul dit eerst in om het boek toe te voegen.")  
+  } 
+  else{
   let boek = new Boek();
   boek.auteur=this.auteur;
+  boek.aantal=this.aantal;
+  boek.isbn=this.isbn;
+  boek.tags=this.tags;
+  boek.status="Beschikbaar";
+  boek.titel=this.titel;
   console.log("this.auteur"+this.auteur)
   this._boekService.addBook(boek).subscribe(()=>{    
-  })
+  })  
+  this.router.navigate(['/boekenzoeken']);
+}
 }
   
 }
